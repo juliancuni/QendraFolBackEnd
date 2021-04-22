495efb167d23cf30eb502a4007c1a8aeb7a17ad6
Julian Cuni <juliancuni@gmail.com>
2021-04-22 22:06:17 +0200

    Init

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApiUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApiUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null) return user;
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiUser>> CreateUser(ApiUser user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+    }
+}
---
+++ Entities/ApiUser.cs
@@ -0,0 +1,8 @@
+namespace Entities
+{
+    public class ApiUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
